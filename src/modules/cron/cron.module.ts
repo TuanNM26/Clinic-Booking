e1aaf566b8d6dea03290875469221a37bd23f7ab
Nmtuan <tuannmhe172118@fpt.edu.vn>
2025-04-15 16:49:39 +0700

    update cron job

---
+++ src/modules/cron/cron.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CronService } from './cron.service';
+import { MailModule } from '../mails/mail.module';
+import { AppointmentsModule } from '../appointments/appointments.module';
+
+@Module({
+  imports: [MailModule,AppointmentsModule], 
+  providers: [CronService],
+  exports: [CronService], 
+})
+export class CronModule {}
